e931da1cef21f6fb8e73153d15ab37248fbe3110
Administrator <manojkmr.kumar894@gmail.com>
2024-10-24 11:45:32 +0530

    add program

---
+++ src/ex_02102024/sum.java
@@ -0,0 +1,10 @@
+package ex_02102024;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = a+ b;
+        System.out.println(sum);
+    }
+}
